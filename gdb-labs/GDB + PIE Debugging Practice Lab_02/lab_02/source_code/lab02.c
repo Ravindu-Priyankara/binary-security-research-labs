d136113d16f74ac1c957fdc85c831fcd3c52421c
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2025-12-10 15:58:52 +0530

    update code for fix compiling error

--- gdb-labs/GDB + PIE Debugging Practice Lab_02/lab_02/source_code/lab02.c
+++ gdb-labs/GDB + PIE Debugging Practice Lab_02/lab_02/source_code/lab02.c
@@ -1,6 +1,9 @@
 /*
-* compile = gcc lab02.c -o lab02 -fPIE -pie -g -fno-stack-protector -z execstack
+* compile = gcc lab02.c -o lab02 -std=c89 -fPIE -pie -g -fno-stack-protector -z execstack
 */
+#define _FORTIFY_SOURCE 0 
+#define _GNU_SOURCE    
+
 
 #include <stdio.h>
 #include <string.h>
