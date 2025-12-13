1a6f718ed79af3425d5a9efc834adf864c8b6d19
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2025-12-13 10:35:50 +0530

    remove unnecessary files

--- shellcode-labs/lab_01_exit_shellcode/v1/exit.asm
+++
@@ -1,8 +0,0 @@
-section .text
-global _start
-
-_start:
-	xor rax, rax	; Make rax register zero
-	mov al, 60	; Syscall number for sys_exit
-	xor rdi, rdi	; First argument: exit status code 0
-	syscall
